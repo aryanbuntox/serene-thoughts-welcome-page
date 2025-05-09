46861746f586bf544575ff5ee644e713ee53fef0
Aryan Pandey <cornhubid1@gmail.com>
2025-05-09 20:42:38 +0530

    Update SocialMediaSlideshow.tsx

--- src/components/SocialMediaSlideshow.tsx
+++ src/components/SocialMediaSlideshow.tsx
@@ -26,7 +26,7 @@ const SocialMediaSlideshow = () => {
 
     const scrollTimer = setInterval(() => {
       emblaApi.scrollNext();
-    }, 800); // Slow scroll: every 3 seconds
+    }, 300); // Slow scroll: every 3 seconds
 
     return () => {
       clearInterval(scrollTimer);
